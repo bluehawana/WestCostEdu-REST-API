3ef9f91f0e851b61e9a34f62b30787dda223e690
Hongzhi Li <bluehawana@gmail.com>
2022-10-05 23:47:37 +0200

    final commit

--- scripts/course-page.js
+++ scripts/course-page.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const ourCourses = document.querySelector(".courses-wrapper");
-const modalDialog = document.querySelector(".modal");
+const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
@@ -13,17 +13,17 @@ const createHtml = (course) => {
       <img src="${course.imageUrl}" id="${course.id}" width="400" height="600" />
       <p>StartDate ${course.startDate} --- EndDate ${course.endDate}</p>
       <p>BookEdition ${course.bookEdition} | Price kr ${course.price}</p>
-      <bottom description="${course.description}" class="btn">More info</bottom>
+      <detail description="${course.description}" class="btn">More info</detail>
     </div>`
   );
 };
 
-const loadBottoms = () => {
-  const bottoms = document.querySelectorAll(".courses-card bottom");
+const loadDetails = () => {
+  const details = document.querySelectorAll(".courses-card detail");
 
-  bottoms.forEach((bottom) => {
-    let description = bottom.getAttribute("description");
-    bottom.addEventListener("click", () => {
+  details.forEach((detail) => {
+    let description = detail.getAttribute("description");
+    detail.addEventListener("click", () => {
       openModal(description);
     });
   });
@@ -33,54 +33,24 @@ const getCourses = () => {
   courses.forEach((course) => {
     createHtml(course);
   });
-  loadBottoms();
-};
-
-const findCourses = () => {
-  const searchValue = searchField.value;
-  if (searchValue === "") {
-    ourCourses.innerHTML = "";
-    getCourses();
-    return;
-  }
-
-  let found;
-  found = courses.filter((course) =>
-    course.title.toUpperCase().startsWith(searchValue.toUpperCase())
-  );
-
-  ourCourses.innerHTML = "";
-  found.forEach((course) => createHtml(course));
+  loadDetails();
 };
 
 const openModal = (e) => {
-  const description = modalDialog.querySelector(".modal-container");
+  const description = modal.querySelector(".modal-container");
   description.innerHTML = `<a>${e}</a>`;
-  modalDialog.classList.remove("hidden");
+  modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
-const quitModal = () => {
-  modalDialog.classList.add("hidden");
+const quitModal = function () {
+  modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
-searchBtn.addEventListener("click", (e) => {
-  findCourses();
-  const images = document.querySelectorAll(".courses-card img");
-});
-
-searchField.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    findCourses();
-  }
-});
-
 closeModal.addEventListener("click", quitModal);
 document.addEventListener("keyup", (e) => {
-  if (e.key === "Escape") {
-    if (!modalDialog.classList.contains("hidden")) quitModal();
-  }
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) quitModal();
 });
 
 getCourses();
